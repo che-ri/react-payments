8ccf42b648a0b331a7bfe26779ebd36aeb5e2460
che-ri <rnjs1135@gmail.com>
2022-02-01 16:13:42 +0900

    feat: 홈으로 이동

--- src/shared/Header/index.jsx
+++ src/shared/Header/index.jsx
@@ -1,15 +1,35 @@
 import React from "react";
 import styles from "./Header.module.css";
+import { useLocation } from "react-router";
+import { useNavigate } from "react-router";
 
 //components
 import HeaderNav from "../../components/HeaderNav";
 import { ReactComponent as ArrowLeft } from "../../asset/icons/arrow-left.svg";
 
 export default function Header() {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
+  function ArrowLeftComponent() {
+    const goHome = () => navigate("/");
+    return (
+      <div onClick={goHome}>
+        <ArrowLeft />
+      </div>
+    );
+  }
+
   return (
     <div className={styles.PageTitle}>
-      <HeaderNav text="보유카드" to="/" />
-      {/* <HeaderNav text="카드상세" to="/detail" iconComponent={<ArrowLeft />} /> */}
+      {pathname === "/" && <HeaderNav text="보유카드" to="/" />}
+      {pathname === "/detail" && (
+        <HeaderNav
+          text="카드상세"
+          to="/detail"
+          iconComponent={<ArrowLeftComponent />}
+        />
+      )}{" "}
     </div>
   );
 }
